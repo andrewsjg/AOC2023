7bc108a91b0b992db69677db0c2f17cbd34cac01
James Andrews <jgandrews@gmail.com>
2023-12-02 01:07:07 +0000

    puzzle 1 complete

--- Puzzle1/puzzle1.go
+++ Puzzle1/puzzle1.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
 
@@ -52,10 +53,92 @@ func P1Solve(input string) {
 		}
 
 		calibrationTotal = calibrationTotal + calVal
-
 	}
 
-	fmt.Println(calibrationTotal)
+	fmt.Printf("The calibration total for puzzle 1, part 1 is: %d\n", calibrationTotal)
+}
+
+func P2Solve(input string) {
+	calibrationTotal := 0
+
+	validDigitStrings := []string{"one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9"}
+
+	digitMap := map[string]int{
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"1":     1,
+		"2":     2,
+		"3":     3,
+		"4":     4,
+		"5":     5,
+		"6":     6,
+		"7":     7,
+		"8":     8,
+		"9":     9,
+	}
+
+	file, err := os.Open(input)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		occurances := make(map[int]string)
+
+		for _, validDigit := range validDigitStrings {
+			firstoccurance := strings.Index(line, validDigit)
+			lastoccurance := strings.LastIndex(line, validDigit)
+
+			if firstoccurance != -1 {
+				occurances[firstoccurance] = validDigit
+			}
+
+			if lastoccurance != -1 {
+				occurances[lastoccurance] = validDigit
+			}
+		}
+
+		highest := 0
+		lowest := 9999999999
+
+		firstDigit := 0
+		lastDigit := 0
+
+		for idx, digit := range occurances {
+
+			if idx <= lowest {
+				lowest = idx
+				firstDigit = digitMap[digit]
+			}
+
+			if idx >= highest {
+				highest = idx
+				lastDigit = digitMap[digit]
+			}
+
+		}
+
+		calVal, err := strconv.Atoi(strconv.Itoa(firstDigit) + strconv.Itoa(lastDigit))
+		if err != nil {
+			fmt.Println("couldnt create a value: " + err.Error())
+			os.Exit(1)
+		}
+
+		calibrationTotal = calibrationTotal + calVal
+	}
+
+	fmt.Printf("The calibration total for puzzle 1, part 2 is: %d\n", calibrationTotal)
+
 }
 
 // Check if the passed rune is a digit then return the digit as an int
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import puzzle1 "github.com/andrewsjg/AOC2023/Puzzle1"
+import (
+	"fmt"
+
+	puzzle1 "github.com/andrewsjg/AOC2023/Puzzle1"
+)
 
 func main() {
+	// Puzzle 1
 	puzzle1.P1Solve("./Puzzle1/input.txt")
+	fmt.Println()
+	//puzzle1.P2Solve("./Puzzle1/sampleinput2.txt")
+	puzzle1.P2Solve("./Puzzle1/input.txt")
 }
