a820dd236d833232495478c8545c9cc8c14df57b
James Andrews <jgandrews@gmail.com>
2023-12-02 01:15:01 +0000

    puzzle 1 complete

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
 import (
-	"fmt"
-
 	puzzle1 "github.com/andrewsjg/AOC2023/Puzzle1"
 )
 
 func main() {
 	// Puzzle 1
 	puzzle1.P1Solve("./Puzzle1/input.txt")
-	fmt.Println()
-	//puzzle1.P2Solve("./Puzzle1/sampleinput2.txt")
 	puzzle1.P2Solve("./Puzzle1/input.txt")
 }
