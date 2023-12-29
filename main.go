b2ba134bdc7a5dc42e33693fcca1fb4a5022d16d
James Andrews <jgandrews@gmail.com>
2023-12-29 10:43:09 +0000

    Puzzle 12 Part 1 - Naieve approach

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/andrewsjg/AOC2023/puzzle1"
 	"github.com/andrewsjg/AOC2023/puzzle10"
 	"github.com/andrewsjg/AOC2023/puzzle11"
+	"github.com/andrewsjg/AOC2023/puzzle12"
 	"github.com/andrewsjg/AOC2023/puzzle2"
 	"github.com/andrewsjg/AOC2023/puzzle3"
 	"github.com/andrewsjg/AOC2023/puzzle4"
@@ -20,64 +21,64 @@ func main() {
 	fmt.Println("======================================================")
 	fmt.Println("                     Puzzle 1")
 	fmt.Println("======================================================")
-	puzzle1.Part1Solve("./Puzzle1/input.txt")
-	puzzle1.Part2Solve("./Puzzle1/input.txt")
+	puzzle1.Part1Solve("./puzzleinput/puzzle1/input.txt")
+	puzzle1.Part2Solve("./puzzleinput/Puzzle1/input.txt")
 
 	fmt.Println()
 	fmt.Println("======================================================")
 	fmt.Println("                     Puzzle 2")
 	fmt.Println("======================================================")
-	puzzle2.Part1Solve("./Puzzle2/input.txt")
-	puzzle2.Part2Solve("./Puzzle2/input.txt")
+	puzzle2.Part1Solve("./puzzleinput/Puzzle2/input.txt")
+	puzzle2.Part2Solve("./puzzleinput/Puzzle2/input.txt")
 
 	fmt.Println()
 	fmt.Println("======================================================")
 	fmt.Println("                     Puzzle 3")
 	fmt.Println("======================================================")
-	puzzle3.Part1Solve("./Puzzle3/input.txt")
-	puzzle3.Part2Solve("./Puzzle3/Input.txt")
+	puzzle3.Part1Solve("./puzzleinput//Puzzle3/input.txt")
+	puzzle3.Part2Solve("./puzzleinput/Puzzle3/Input.txt")
 
 	fmt.Println()
 	fmt.Println("======================================================")
 	fmt.Println("                     Puzzle 4")
 	fmt.Println("======================================================")
-	puzzle4.Part1Solve("./Puzzle4/input.txt")
-	puzzle4.Part2Solve("./Puzzle4/input.txt")
+	puzzle4.Part1Solve("./puzzleinput/Puzzle4/input.txt")
+	puzzle4.Part2Solve("./puzzleinput//Puzzle4/input.txt")
 
 	fmt.Println()
 	fmt.Println("======================================================")
 	fmt.Println("                     Puzzle 5")
 	fmt.Println("======================================================")
 	fmt.Println("Day 5 solution takes to long to run!")
-	//puzzle5.Part1Solve("./Puzzle5/input.txt")
-	//puzzle5.Part2Solve("./Puzzle5/input.txt")
+	//puzzle5.Part1Solve("./puzzleinput/Puzzle5/input.txt")
+	//puzzle5.Part2Solve("./puzzleinput/Puzzle5/input.txt")
 
 	fmt.Println()
 	fmt.Println("======================================================")
 	fmt.Println("                     Puzzle 6")
 	fmt.Println("======================================================")
-	puzzle6.Part1Solve("./puzzle6/input.txt")
-	puzzle6.Part2Solve("./puzzle6/input.txt")
+	puzzle6.Part1Solve("./puzzleinput//puzzle6/input.txt")
+	puzzle6.Part2Solve("./puzzleinput/puzzle6/input.txt")
 
 	fmt.Println()
 	fmt.Println("======================================================")
 	fmt.Println("                     Puzzle 7")
 	fmt.Println("======================================================")
-	puzzle7.Part1Solve("./puzzle7/input.txt")
-	puzzle7.Part2Solve("./puzzle7/input.txt")
+	puzzle7.Part1Solve("./puzzleinput/puzzle7/input.txt")
+	puzzle7.Part2Solve("./puzzleinput/puzzle7/input.txt")
 
 	fmt.Println()
 	fmt.Println("======================================================")
 	fmt.Println("                     Puzzle 8")
 	fmt.Println("======================================================")
-	puzzle8.Part1Solve("./puzzle8/input.txt")
-	puzzle8.Part2Solve("./puzzle8/input.txt")
+	puzzle8.Part1Solve("./puzzleinput/puzzle8/input.txt")
+	puzzle8.Part2Solve("./puzzleinput/puzzle8/input.txt")
 
 	fmt.Println()
 	fmt.Println("======================================================")
 	fmt.Println("                     Puzzle 9")
 	fmt.Println("======================================================")
-	puzzle9.Part1Solve("./puzzle9/input.txt")
+	puzzle9.Part1Solve("./puzzleinput/puzzle9/input.txt")
 
 	fmt.Println("Puzzle 9 part 2 takes a beat to long")
 	// puzzle9.Part2Solve("./puzzle9/input.txt")
@@ -86,14 +87,21 @@ func main() {
 	fmt.Println("======================================================")
 	fmt.Println("                     Puzzle 10")
 	fmt.Println("======================================================")
-	puzzle10.Part1Solve("./puzzle10/input.txt")
-	puzzle10.Part2Solve("./puzzle10/input.txt")
+	puzzle10.Part1Solve("./puzzleinput/puzzle10/input.txt")
+	puzzle10.Part2Solve("./puzzleinput/puzzle10/input.txt")
 
 	fmt.Println()
 	fmt.Println("======================================================")
 	fmt.Println("                     Puzzle 11")
 	fmt.Println("======================================================")
-	puzzle11.Part1Solve("./puzzle11/input.txt")
-	puzzle11.Part2Solve("./puzzle11/input.txt")
+	puzzle11.Part1Solve("./puzzleinput/puzzle11/input.txt")
+	puzzle11.Part2Solve("./puzzleinput/puzzle11/input.txt")
+
+	fmt.Println()
+	fmt.Println("======================================================")
+	fmt.Println("                     Puzzle 12")
+	fmt.Println("======================================================")
+	puzzle12.Part1Solve("./puzzleinput/puzzle12/sampleinput.txt")
+	// puzzle12.Part2Solve("./puzzleinput/puzzle12/input.txt")
 
 }
